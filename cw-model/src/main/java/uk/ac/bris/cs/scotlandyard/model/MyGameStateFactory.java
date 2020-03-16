ab0ae54782fe8b8ee5cdeb28163df72a64498009
kp19251 <kp19251@bristol.ac.uk>
2020-03-16 16:46:11 +0000

    first 3 testsin GameStateCreationTest.java

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,8 +4,12 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * cw-model
@@ -17,9 +21,67 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
+            return new MyGameState(setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
+
+	}
 		// TODO
-		throw new RuntimeException("Implement me!");
+		 private final class MyGameState implements GameState {
+		    private GameSetup setup;
+		    private ImmutableSet<Piece> remaining;
+		    private ImmutableList<LogEntry> log;
+		    private Player mrX;
+		    private List<Player> detectives;
+		    private ImmutableList<Player> everyone;
+		    private ImmutableSet<Move> moves;
+		    private ImmutableSet<Piece> winner;
+			private MyGameState(final GameSetup setup, final ImmutableSet<Piece> remaining,final ImmutableList<LogEntry> log, final Player mrX, final List<Player> detectives){
+				 if(setup == null || remaining == null || log == null || mrX == null || detectives ==null) throw new NullPointerException("1234");
+			     this.setup = setup;
+               ;
+                 this.remaining = remaining;
+
+                 this.log = log;
+
+                 this.mrX = mrX;
+
+			     this.detectives = detectives;
+			}
+			@Override
+			public GameSetup getSetup() {
+				return null;
+			}
+
+
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
+				return null;
+			}
+
+
+
+			@Override
+			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				return null;
+			}
+
+
+
+			@Override
+			public Optional<Board.TicketBoard> getPlayerTickets(Piece piece){return null;}
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog(){return null;}
+			@Override
+			public ImmutableSet<Piece> getWinner(){return null;};
+			@Override
+			public ImmutableSet<Move> getAvailableMoves(){return null;}
+			@Override
+			public GameState advance(Move move) { return null; }
+
+		}
+
+
+
 
 	}
 
-}
+
