719fb6c7104bc4c396c360abd6a81cb3f1bd97dd
kp19251 <kp19251@bristol.ac.uk>
2020-04-01 15:23:44 +0100

    20 tests, need to do 22 test by tonight

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -42,6 +42,10 @@ public final class MyGameStateFactory implements Factory<GameState> {
 				 CheckMoreThanOneMrX(detectives);
                  CheckDuplicateDetectives(detectives);
                  CheckLocationOverLapBetweenDetectives(detectives);
+                 CheckTicketsDetectives(detectives);
+                 CheckEmptyRoundsAndGraph(setup);
+
+
 			     this.setup = setup;
                  this.remaining = remaining;
                  this.log = log;
@@ -50,22 +54,32 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			}
 			@Override
 			public GameSetup getSetup() {
-				return null;
+				return setup;
 			}
 			@Override
 			public ImmutableSet<Piece> getPlayers() {
-				return null;
+				List<Piece> PieceList = new ArrayList<>();
+				PieceList.add(mrX.piece());
+				for (Player player : detectives){
+					PieceList.add(player.piece());
+				}
+				return ImmutableSet.copyOf(PieceList);
 			}
 			@Override
 			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
-				return null;
+                for (final var p : detectives){
+                	if(p.piece() == detective) return Optional.of(p.location());
+				}
+				return Optional.empty();
 			}
 			@Override
 			public Optional<Board.TicketBoard> getPlayerTickets(Piece piece){return null;}
 			@Override
 			public ImmutableList<LogEntry> getMrXTravelLog(){return null;}
 			@Override
-			public ImmutableSet<Piece> getWinner(){return null;};
+			public ImmutableSet<Piece> getWinner(){
+				return ImmutableSet.of();
+			}
 			@Override
 			public ImmutableSet<Move> getAvailableMoves(){return null;}
 			@Override
@@ -85,8 +99,23 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 			}
 			public void CheckLocationOverLapBetweenDetectives(final List<Player> detectives){
-				
+				List<Integer> list = new ArrayList<>();
+				for (Player player : detectives){
+					if(list.contains(player.location())) throw new IllegalArgumentException();
+					if(player != null) list.add(player.location());
+				}
 			}
+			public void CheckTicketsDetectives(final List<Player> detectives){
+				for (Player player : detectives){
+					if(player.has(ScotlandYard.Ticket.SECRET)) throw new IllegalArgumentException();
+					if(player.has(ScotlandYard.Ticket.DOUBLE)) throw new IllegalArgumentException();
+				}
+			}
+			public void CheckEmptyRoundsAndGraph(final GameSetup setup){
+                 if (setup.rounds.size() <= 0) throw new IllegalArgumentException();
+                 if (setup.graph.nodes().size() <= 0) throw new IllegalArgumentException();
+			}
+
 		}
 
 
